7b3058f758852228d81094f69fa199818ed59fc0
Lauvernier <timothe@lauvernier.fr>
2023-06-16 23:58:29 +0200

    add json file

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -7,8 +7,7 @@ function Portfolio() {
     <div>
       <div className="font-extrabold text-3xl">MES PROJETS</div>
       <div className="mt-8 flex items-center flex-row flex-wrap lg:flex-nowrap space-x-0 space-y-2 lg:space-x-2 lg:space-y-0 p-5">
-        <BackgroundArrow image={ohmyfoodLogo} imageExpend={ohmyfood} name={"OHMYFOOD"} />
-        <BackgroundArrow imageExpend={Panthere} name={"LA PANTHERE"} />
+        <BackgroundArrow/>
       </div>
     </div>
   );
--- src/component/BackgroundArrow/BackgroundArrow.jsx
+++ src/component/BackgroundArrow/BackgroundArrow.jsx
@@ -1,26 +1,31 @@
 import React, { useState } from "react";
+import Portfolio from '../../Datas/Portfolio.json'
 import "./BackgroundArrow.css";
 
-function BackgroundArrow({image, imageExpend, name }) {
+function BackgroundArrow({ image, imageExpend }) {
   const [isExpanded, setIsExpanded] = useState(false);
   const [isPreviewing, setIsPreviewing] = useState(false);
+  const elements = Portfolio.elements
 
   const handlePreviewClick = () => {
     setIsPreviewing(!isPreviewing);
   };
 
   return (
-    <div
-      className={`container border-black border-2 bg-cover flex justify-center items-center`}
-      onClick={() => setIsExpanded(!isExpanded)}
-    >
-      {isExpanded && (
-        <div className="preview-overlay" onClick={handlePreviewClick}>
-          <img src={imageExpend} alt={name} className="preview-image" />
-        </div>
-      )}
-     <img src={image} alt={name}/>
-    </div>
+    elements.map((obj, index) => (
+      <div
+        className={`${obj.name} container border-black border-2 bg-cover flex justify-center items-center`}
+        onClick={() => setIsExpanded(!isExpanded)}
+        key={obj.name + index}
+      >
+        {isExpanded && (
+          <div className="preview-overlay" onClick={handlePreviewClick}>
+            <img src={imageExpend} alt={obj.name} className="preview-image" />
+          </div>
+        )}
+        <img src={image} alt={obj.name} />
+      </div>
+    ))
   );
 }
 
