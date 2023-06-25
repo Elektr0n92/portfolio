dbad81331382e33fe26b2f270bfff235aa0b6f45
Timothé Lauvernier <timothe@lauvernier.fr>
2023-06-25 18:05:02 +0200

    configure capcha

--- src/component/mail/Mail.jsx
+++ src/component/mail/Mail.jsx
@@ -1,27 +1,44 @@
 import React, { useEffect, useState, useRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import emailjs from "@emailjs/browser";
+import { RingLoader } from "react-spinners";
 import "./Mail.css";
 const template = process.env.REACT_APP_EMAILJS_TEMPLATEID;
 const service = process.env.REACT_APP_EMAILJS_SERVICEID;
 const publicKey = process.env.REACT_APP_EMAILJS_PUBLICKEY;
-console.log(service);
+const publicKeyRecapcha = process.env.REACT_APP_RECAPCHA;
+
 const ContactUs = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState("");
-
-  fetch("http://localhost:3001/api/hello")
-    .then((response) => response.json())
-    .then((data) => setMessage(data.message))
-    .catch((error) => console.log(error));
   const form = useRef();
+  const recaptchaRef = useRef();
 
-  const sendEmail = (e) => {
+  useEffect(() => {
+    fetch("http://localhost:3001/api/hello")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleFormSubmit = (e) => {
     e.preventDefault();
+
+    // Valider le reCAPTCHA ici
+    const recaptchaValue = recaptchaRef.current.getValue();
+    if (!recaptchaValue) {
+      alert("Veuillez compléter le reCAPTCHA.");
+      return;
+    }
+
+    setIsLoading(true);
 
     emailjs.sendForm(service, template, form.current, publicKey).then(
       (result) => {
         console.log(result.text);
         alert("Message envoyé");
         form.current.reset();
+        setIsLoading(false);
       },
       (error) => {
         console.log(error.text);
@@ -31,9 +48,14 @@ const ContactUs = () => {
 
   return (
     <>
+      {isLoading && (
+        <div className="spinner-container">
+          <RingLoader size={100} color={"#123abc"} />
+        </div>
+      )}
       <form
         ref={form}
-        onSubmit={sendEmail}
+        onSubmit={handleFormSubmit}
         className="flex flex-col gap-2 w-full"
       >
         <label>Nom</label>
@@ -42,6 +64,16 @@ const ContactUs = () => {
         <input type="email" name="user_email" required />
         <label>Message</label>
         <textarea name="message" required />
+        <div className="flex justify-center">
+          <ReCAPTCHA
+            ref={recaptchaRef}
+            sitekey={publicKeyRecapcha}
+            theme="dark"
+            onChange={() => {
+              // Gérer l'événement onChange du captcha si nécessaire
+            }}
+          />
+        </div>
         <input type="submit" value="Send" className="text-white border mt-5" />
       </form>
     </>
