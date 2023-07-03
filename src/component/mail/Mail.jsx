12f883c18b1f23e994e3c643868708f1742f0992
Lauvernier <timothe@lauvernier.fr>
2023-07-03 21:03:11 +0200

    Chnage send to ENVOYER

--- src/component/mail/Mail.jsx
+++ src/component/mail/Mail.jsx
@@ -77,7 +77,7 @@ const ContactUs = () => {
         </div>
         <input
           type="submit"
-          value="Send"
+          value="ENVOYER"
           className="text-white border mt-5 cursor-pointer hover:ring-2 ring-white hover:ring-opacity-50"
         />
       </form>
