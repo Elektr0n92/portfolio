9d73b37961303a7b08ae6157b7ba2233d66a1a74
Timothé Lauvernier <timothe@lauvernier.fr>
2023-07-03 20:44:08 +0200

    add _blank target for cv

--- src/Pages/Cv/Cv.jsx
+++ src/Pages/Cv/Cv.jsx
@@ -3,7 +3,7 @@ import "./Cv.css";
 function Cv() {
   return (
     <div className="max-h-full">
-      <a href={CV} alt="CV">
+      <a href={CV} alt="CV" target="_blank" rel="noreferrer">
         <img src={CV} alt="CV" className="max-h-full" />
       </a>
     </div>
