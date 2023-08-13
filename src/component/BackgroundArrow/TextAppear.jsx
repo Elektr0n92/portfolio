d0292e4765f1e84c32e9d53716f012f4b2b1ffe4
Timothé Lauvernier <timothe@lauvernier.fr>
2023-08-13 04:20:21 +0200

    add correction

--- src/component/BackgroundArrow/TextAppear.jsx
+++ src/component/BackgroundArrow/TextAppear.jsx
@@ -1,17 +1,17 @@
 import "./BackgroundArrow.css";
 
 function TextAppear({ title, description }) {
-  const formattedDescription = description.map((desc, index) => (
-    <div key={index}>
-      <p>{desc.firstline}</p>
-      <p>{desc.after.join(" ")}</p>
-    </div>
-  ));
-
   return (
     <div className="flex top-0 absolute animate-slideRight w-full transform -translate-y-1/2 text-appear opacity-0 flex-col border border-white h-full">
       <div className="text-center">{title}</div>
-      <div className="animate-TextAppear opacity-0">{formattedDescription}</div>
+      <div className="animate-TextAppear opacity-0">
+        {description.map((desc, index) => (
+          <div key={index} className="flex gap-2 flex-col">
+            <div className="firstline">{desc.firstline}</div>
+            <div className="after">{desc.after.join(" ")}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
